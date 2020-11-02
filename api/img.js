1c5cb09c0c553e9d949c854f84df870e202df92b
Steven <steven@ceriously.com>
2020-11-02 11:27:41 -0500

    Fix index page

--- api/img.js
+++ api/img.js
@@ -1,10 +1,7 @@
-const deviceWidths = [320, 375, 414, 480, 640, 828, 1024, 1080, 1112, 1200, 1334, 1536, 1668, 1920, 2070, 2224, 2470, 2482, 3000]
-const srcset = deviceWidths.map(w => `/api/img${w} ${w}w`)
-
 module.exports = function handler(req, res) {
-	const match = /img([0-9]+)/.exec(req.url)
-	if (match && match[1]) {
-		const size = match[1]
+  const url = new URL(req.url, 'https://example.com')
+  const size = url.searchParams.get('w')
+	if (size) {
 		const accept = req.headers['accept']
 		const userAgent = req.headers['user-agent']
 		console.log({ size, accept, userAgent })
@@ -18,23 +15,9 @@ module.exports = function handler(req, res) {
 			<text x="${pos}" y="${pos + 200}" style="${style2}">Accept: ${accept}</text>
 			<text x="${pos}" y="${pos + 300}" style="${style2}">User-Agent: ${userAgent}</text>
 		</svg>`)
-	} else if (req.url === '/') {
-		res.setHeader('Content-Type', 'text/html')
-		res.end(`<html>
-		<head>
-			<meta name="viewport" content="width=device-width"/>
-			<meta charSet="utf-8"/>
-			<style>img { width:100vw; height:auto; }</style>
-		</head>
-		<body>
-			<img src="/fallback.svg" srcset="${srcset}" />
-		</body>
-		</html>
-		`)
 	} else {
 		res.statusCode = 404
 		res.end('Not Found')
 	}
-
 }
 

c956d3b9ba03cdb15a2b5e9e7421a3f25fc34220
Steven <steven@ceriously.com>
2025-04-22 09:12:54 -0400

    feat: add `accept-ch`

--- api/img.js
+++ api/img.js
@@ -3,8 +3,9 @@ module.exports = function handler(req, res) {
   const size = url.searchParams.get('w')
 	if (size) {
 		const accept = req.headers['accept']
+		const acceptCh = req.headers['accept-ch']
 		const userAgent = req.headers['user-agent']
-		console.log({ size, accept, userAgent })
+		console.log({ size, accept, acceptCh, userAgent })
 		const pos = 0
 		const xmlns = 'http://www.w3.org/2000/svg'
 		const style1 = 'font: bold 50px monospace'
@@ -13,7 +14,8 @@ module.exports = function handler(req, res) {
 		res.end(`<svg xmlns="${xmlns}" version="1.1" width="${size}" height="${size}">
 			<text x="${pos}" y="${pos + 100}" style="${style1}">Width: ${size}px</text>
 			<text x="${pos}" y="${pos + 200}" style="${style2}">Accept: ${accept}</text>
-			<text x="${pos}" y="${pos + 300}" style="${style2}">User-Agent: ${userAgent}</text>
+			<text x="${pos}" y="${pos + 300}" style="${style2}">Accept: ${acceptCh}</text>
+			<text x="${pos}" y="${pos + 400}" style="${style2}">User-Agent: ${userAgent}</text>
 		</svg>`)
 	} else {
 		res.statusCode = 404

1b40d7bea674f2faf8e94adfd319574209de4cab
Steven <steven@ceriously.com>
2025-04-22 10:02:40 -0400

    feat: add `accept-ch` (#1)

--- api/img.js
+++ api/img.js
@@ -1,23 +1,36 @@
+// @ts-check
+
+function getHeader(req, name) {
+	let value = req.headers[name.toLowerCase()]
+	if (value === undefined) {
+		value = ''
+	}
+	return `${name}: ${value}`
+}
+
 module.exports = function handler(req, res) {
   const url = new URL(req.url, 'https://example.com')
+	const headers = req.headers
   const size = url.searchParams.get('w')
-	if (size) {
-		const accept = req.headers['accept']
-		const userAgent = req.headers['user-agent']
-		console.log({ size, accept, userAgent })
-		const pos = 0
-		const xmlns = 'http://www.w3.org/2000/svg'
-		const style1 = 'font: bold 50px monospace'
-		const style2 = 'font: 30px monospace'
-		res.setHeader('Content-Type', 'image/svg+xml')
-		res.end(`<svg xmlns="${xmlns}" version="1.1" width="${size}" height="${size}">
-			<text x="${pos}" y="${pos + 100}" style="${style1}">Width: ${size}px</text>
-			<text x="${pos}" y="${pos + 200}" style="${style2}">Accept: ${accept}</text>
-			<text x="${pos}" y="${pos + 300}" style="${style2}">User-Agent: ${userAgent}</text>
-		</svg>`)
-	} else {
+	if (!size) {
 		res.statusCode = 404
 		res.end('Not Found')
+		return
 	}
+	let pos = 0
+	const xmlns = 'http://www.w3.org/2000/svg'
+	const style1 = 'font: bold 50px monospace'
+	const style2 = 'font: 30px monospace'
+	res.setHeader('accept-ch', 'downlink, ect, rtt, save-data')
+	res.setHeader('content-type', 'image/svg+xml')
+	res.end(`<svg xmlns="${xmlns}" version="1.1" width="${size}" height="${size}">
+		<text x="0" y="${pos += 75}" style="${style1}">Width: ${size}px</text>
+		<text x="0" y="${pos += 75}" style="${style2}">${getHeader(req, 'Accept')}</text>
+		<text x="0" y="${pos += 75}" style="${style2}">${getHeader(req, 'Downlink')}</text>
+		<text x="0" y="${pos += 75}" style="${style2}">${getHeader(req, 'ECT')}</text>
+		<text x="0" y="${pos += 75}" style="${style2}">${getHeader(req, 'RTT')}</text>
+		<text x="0" y="${pos += 75}" style="${style2}">${getHeader(req, 'Save-Data')}</text>
+		<text x="0" y="${pos += 75}" style="${style2}">${getHeader(req, 'User-Agent')}</text>
+	</svg>`)
 }
 
